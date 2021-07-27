a68908599ef9d6bd4d1b818fe485eeaad155b880
Mariia Berger <slackerpanda@gmail.com>
2021-07-27 13:18:04 +0300

    Initial Commit

---
+++ hw2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2.7
+//
+//  Created by Maria Berger on 27.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
